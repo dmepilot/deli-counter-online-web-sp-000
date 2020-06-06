71ad1300b4cb9b16b0c10548b2fa51189d736140
Dean Erickson <dmepilot@yahoo.com>
2020-06-06 19:36:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(current_line)
 end
 
 def take_a_number(current_line, new_person)
-  
+  puts "Welcome #{new_person}, you are numer #{(current_line<<new_person).size} in line."
 end
 
 def now_serving
