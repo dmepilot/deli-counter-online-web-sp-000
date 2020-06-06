444c1998f1775bebfd8e2fe41a5687bf9a3e2cfb
Dean Erickson <dmepilot@yahoo.com>
2020-06-06 17:17:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,11 @@
 # Write your code here.
 def line(current_line)
-  current_line.collect do |position|
-    counter = 1
-    puts counter". #{position.join}"
-    counter += 1
+ 
   if current_line == []
     puts "The line is currently empty."
   elsif current_line != []
     puts "The line is currently: "
+    
   end
   end
 end
