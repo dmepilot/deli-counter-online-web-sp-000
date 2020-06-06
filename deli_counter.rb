295ea277a0b77255c4fb01e2ba50d065fc65d01e
Dean Erickson <dmepilot@yahoo.com>
2020-06-06 18:36:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,7 @@ def line(current_line)
   if current_line == []
     puts "The line is currently empty."
   elsif current_line != []
-     name = current_line.each do |position|
-    end
-      puts "The line is currently: #{name}"
- 
+      puts "The line is currently: #{current_line.join(" ")} "
   end
 end
 
